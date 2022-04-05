a2fb39f384e01acd8273f5f5bf7cfcf719bde134
Corma Martinez del Rio <66973815+cmdelrio@users.noreply.github.com>
2022-04-05 14:56:58 -0500

    Update transform_mobilize_data.sql

--- transform_mobilize_data.sql
+++ transform_mobilize_data.sql
@@ -3,7 +3,7 @@
 -- It creates this table from the participations table by
 -- using the DISTINCT SQL function
 
-CREATE TABLE cormac_scratch.mobilize_users_to_sync AS (
+CREATE TABLE mobilize_schema.mobilize_users_to_sync AS (
 
 SELECT DISTINCT
     user_id as mobilizeid
@@ -12,7 +12,7 @@ SELECT DISTINCT
   , email_address
   , phone_number
   , postal_code
-FROM cormac_scratch.mobilize_participations as mob
+FROM mobilize_schema.mobilize_participations as mob
 -- Joining the log table lets us know which records have been synced
 -- and which records still need to be synced
 LEFT JOIN cormac_scratch.mobilize_to_actionnetwork_log as log 
