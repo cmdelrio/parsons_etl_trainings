0a4ebf2125fda10f7bf6ee069f0018678cce1805
Corma Martinez del Rio <66973815+cmdelrio@users.noreply.github.com>
2022-04-05 14:55:49 -0500

    Create transform_mobilize_data.sql

---
+++ transform_mobilize_data.sql
@@ -0,0 +1,22 @@
+-- This script creates a table where each row is a unique
+-- Mobilize user that needs to be synced to Action Network
+-- It creates this table from the participations table by
+-- using the DISTINCT SQL function
+
+CREATE TABLE cormac_scratch.mobilize_users_to_sync AS (
+
+SELECT DISTINCT
+    user_id as mobilizeid
+  , given_name 
+  , family_name 
+  , email_address
+  , phone_number
+  , postal_code
+FROM cormac_scratch.mobilize_participations as mob
+-- Joining the log table lets us know which records have been synced
+-- and which records still need to be synced
+LEFT JOIN cormac_scratch.mobilize_to_actionnetwork_log as log 
+  on log.mobilizeid = mob.user_id
+WHERE log.synced is null
+
+);
